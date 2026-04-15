02a1247f016ecd97fc0686e15493870fcc09d533
Nikhil-bhargav-reddy <74861413+Nikhil-bhargav-reddy@users.noreply.github.com>
2026-04-14 21:21:04 -0400

    Add: kth-largest-element-in-an-array - submission-5

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-5.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        import heapq
+
+        nums_h = []
+
+        heapq.heapify(nums)
+
+        while len(nums)>k:
+            heapq.heappop(nums)
+
+        return nums[0]
+
+
