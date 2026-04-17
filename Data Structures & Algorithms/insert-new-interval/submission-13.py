988ce36cb71897a952f741a66839a07ec86583dd
Nikhil-bhargav-reddy <74861413+Nikhil-bhargav-reddy@users.noreply.github.com>
2026-04-17 15:43:23 -0400

    Add: insert-new-interval - submission-13

---
+++ Data Structures & Algorithms/insert-new-interval/submission-13.py
@@ -0,0 +1,25 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+        l = 0
+        n = len(intervals)
+        
+        # 1. The "Before" phase
+        while l < n and intervals[l][1] < newInterval[0]:
+            res.append(intervals[l])
+            l += 1
+            
+        # 2. The "During / Overlap" phase (Your brilliant negation logic!)
+        while l < n and not (newInterval[1] < intervals[l][0]):
+            newInterval[0] = min(intervals[l][0], newInterval[0])
+            newInterval[1] = max(intervals[l][1], newInterval[1])
+            l += 1
+            
+        res.append(newInterval)
+        
+        # 3. The "After" phase
+        while l < n:
+            res.append(intervals[l])
+            l += 1
+            
+        return res
