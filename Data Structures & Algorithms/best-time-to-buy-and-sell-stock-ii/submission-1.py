aa370bce8f179d4a6446903c8facc5b211559772
Nikhil-bhargav-reddy <74861413+Nikhil-bhargav-reddy@users.noreply.github.com>
2026-08-09 21:05:38 -0400

    Add: best-time-to-buy-and-sell-stock-ii - submission-1

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # the idea is, we use two pointers l,r 
+        # l holds small values posi
+        # r holds large values posi
+        # if l<r: we should be fine to do difference and add it to our cumsum
+        # if l>r: we move l to r's position, r to l+1
+        
+        l,r = 0,1
+        cum_sum = 0
+
+        while r<len(prices):
+
+            if prices[l] <prices[r]:
+                diff = prices[r] - prices[l]
+                cum_sum+=diff
+                # now that we have added difference, we should move both left and right by one
+                # we should be fine to move both as we dont want to use same small day again
+                l+=1
+                r+=1
+            else:
+                l = r
+                r+=1
+                # if l>r, then we move l to r's positon and incrmeent r by one
+        
+        return cum_sum
