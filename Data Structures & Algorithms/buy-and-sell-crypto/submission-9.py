8c4c8a10b18560a454de05c75dd3ea14f237a291
Nikhil-bhargav-reddy <74861413+Nikhil-bhargav-reddy@users.noreply.github.com>
2026-08-09 20:55:13 -0400

    Add: buy-and-sell-crypto - submission-9

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-9.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # start with l,r at 0,1 l will be buy date, r will be sell
+        # we need max profit, so we kepe checking whereever we have l<r and increment r as l is smaller
+
+        l,r = 0,1
+
+        max_p = 0
+
+        while r<len(prices):
+            # print(prices[r], prices[l])
+
+
+            if prices[l] <=prices[r]:
+
+                max_p = max(prices[r] - prices[l], max_p)
+                # now move r because l is at smaller
+                r+=1
+            else:
+                print(l,r)
+                l+=1
+                r = l+1
+        return max_p
